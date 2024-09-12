bec5e16a48931d7e085561f28d5c795a66089267
ni9216 <nicholasliech851@gmail.com>
2024-09-12 14:04:13 +0300

    commit

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
-def oxford_comma(items):
-    return None
+
+def oxford_comma(elements):
+    if len(elements) == 0:
+        return ""
+    elif len(elements) == 1:
+        return elements[0]
+    elif len(elements) == 2:
+        return f"{elements[0]} and {elements[1]}"
+    else:
+        oxford_elements = ", ".join(elements[:-1])
+        return f"{oxford_elements}, and {elements[-1]}"
